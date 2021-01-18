9fa4238950693ee130479f571c9d0e7da05cb40f
clement <clementcamara@gmail.com>
2021-01-18 17:07:19 +0100

    test 2

--- streamlit_france.py
+++ streamlit_france.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+@st.cache(allow_output_mutation=True)
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 import seaborn as sns
